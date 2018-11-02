736e76bd1998ac0e7da1c2465ff80735d77d2704
Federico Maidan <fede.maidan@gmail.com>
2018-11-02 06:54:55 -0300

    try catch team controller

--- src/controller/TeamController.php
+++ src/controller/TeamController.php
@@ -20,41 +20,57 @@ class TeamController extends BaseController
     
 	public function createAction($params)
     {
-    	$team = TeamFactory::createTeam($params["name"]);
-        echo $this->message(true, "Team created!" , $team->toArray() );
+        try {
+            $team = TeamFactory::createTeam($params["name"]);
+            echo $this->message(true, "Team created!" , $team->toArray() );
+        } catch(\Exception $e) {
+            echo $this->message(true, $e->getMessage(), null);   
+        }
     }
 
     public function getAction($params)
     {
-        $id = null;
-        if ($params["id"])
-            $id=$params["id"];
-        
-        $teams = DataSingleton::Instance()->getTeams($id);
-        echo $this->message(true, "" , $teams );
+        try {
+            $id = null;
+            if ($params["id"])
+                $id=$params["id"];
+            
+            $teams = DataSingleton::Instance()->getTeams($id);
+            echo $this->message(true, "" , $teams );
+        } catch(\Exception $e) {
+            echo $this->message(true, $e->getMessage(), null);      
+        }
     }
 
     public function editAction($params)
     {
-        if ($params["id"] ) {
-            $id = $params["id"];
-            $updates["name"] = $params["name"];
-            $team = TeamFactory::updateTeam($id, $updates);
-            echo $this->message(true, "Team updated successfully" ,$team);
-        }
-        else {
-            echo $this->message(false, "Id is required", null);
+        try {
+            if ($params["id"] ) {
+                $id = $params["id"];
+                $updates["name"] = $params["name"];
+                $team = TeamFactory::updateTeam($id, $updates);
+                echo $this->message(true, "Team updated successfully" ,$team);
+            }
+            else {
+                echo $this->message(false, "Id is required", null);
+            }
+        } catch(\Exception $e) {
+            echo $this->message(true, $e->getMessage(), null);      
         }
     }
 
     public function deleteAction($params)
     {
-        if ($params["id"] ) {
-            $id = $params["id"];
-            TeamFactory::deleteTeam($id);
-        }
-        else {
-            echo $this->message(false, "Id is required", null);
+        try {
+            if ($params["id"] ) {
+                $id = $params["id"];
+                TeamFactory::deleteTeam($id);
+            }
+            else {
+                echo $this->message(false, "Id is required", null);
+            }   
+        } catch (Exception $e) {
+            echo $this->message(true, $e->getMessage(), null);         
         }
     }
 }
